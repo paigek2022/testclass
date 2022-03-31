45708e57f4bae255ce118a713bcea8f76dc11d39
sxx105 <sxx105@RR100087Stud.UTSARR.NET>
2022-03-31 14:53:40 -0500

    new feature added

--- EgitTesting/src/HelloWorld.java
+++ EgitTesting/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello World");
 		System.out.println("New line is being added");
-		System.out.println("This is new feature addition");
+		System.out.println("Thi is new feature addition");
 	}
 
 }
